114f84b8c5a6e064abf496d5ef03b254adc57f07
Esther00-bala <76984987+Esther00-bala@users.noreply.github.com>
2021-02-01 11:42:28 +0530

    Update res.php

--- WebApp/restaurent/res.php
+++ WebApp/restaurent/res.php
@@ -5,7 +5,17 @@ foreach ($xml->res as $res)
 echo $res->res_id. "<br> " ;
 echo $res->name . "<br> ";
 echo $res->address ."<br>";
-echo $res->dishes ."<br> ";
 echo $res->rating . "<br> ";
+foreach ($res->dishes as $dishes)
+{
+foreach ($dishes->dish as $dish)
+{
+echo $dish->dish_name ."<br> ";
+echo $dish->dish_id ."<br> ";
+echo $dish->dish_cat ."<br> ";
+echo $dish->price ."<br> ";
+
+}
+}
 } 
 ?>
