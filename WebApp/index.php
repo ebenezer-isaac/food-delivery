0254bc2404a57a206ccbaca558ffa8debea51dfd
Ebenezer Isaac <mail@ebenezer-isaac.com>
2021-02-02 18:22:24 +0530

    arrangement

--- WebApp/index.php
+++ WebApp/index.php
@@ -1,79 +1,212 @@
-<?php
-$page = "Organizer Index";
-#include "logger.php";
-session_start();
-if (isset($_SESSION["customer_id"])) {
-    echo "<script>window.location.replace('main.php');</script>";
-} else {
-    ?><html lang="en" >
-        <head>
-            <style>.tooltipp .tooltiptext{visibility: hidden; background-color: #0d0d0d; font-size: 12px; color: #fff; text-align: center; border-radius: 6px; padding: 7px; /* Position the tooltip */ z-index: 1;}.tooltipp:hover .tooltiptext{visibility: visible;}body{height: 100%; overflow-y: scroll;}/* Hide scrollbar for Chrome, Safari and Opera */ ::-webkit-scrollbar{display: none;}/* Hide scrollbar for IE and Edge */ .abt:hover{text-decoration: none;}</style>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width,height=device-height, initial-scale=1">
-            <title>Login </title>
-            <script src="https://kit.fontawesome.com/1ca2da442a.js" crossorigin="anonymous"></script>
-            <link rel="stylesheet" href="css/shakeEffect.css">
-            <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
-            <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-            <link rel="stylesheet" href="css/bootstrap-grid.css" type="text/css">
-            <link rel="stylesheet" href="css/bootstrap-grid.min.css" type="text/css">
-            <link rel="stylesheet" href="css/anim.css" type="text/css">
-            <link rel="stylesheet" href="css/loader-index.css" type="text/css">
-            <link rel="stylesheet" href="css/splash.css" type="text/css">
-            <link rel="icon" href="img/logo.png" type="image/gif">
-            <script src='https://www.google.com/recaptcha/api.js?onload=recaptchaOnload&render=explicit' async defer></script> 
-        </head>
-        <body class='unselectable'>
-            <div id="demo-content">
-                <div id='splash-wrapper'>
-                    <div id='splash'></div>
-                    <div class='splash-section section-left'></div>
-                    <div class='splash-section section-right'></div>
-                </div>
-                <div id="content">
-                    <div class="wrapper">
-                        <div class="main_body">
-                            <div class="container custom_container">
-                                <div id='loginform'></div>
-                            </div><br><br><br><br>
-                        </div>
-                    </div>
-                    <div id="particles-js"></div>
-                    <div class="container">
-                        <div class="modal custom_modal_recaptcha" id="modalrecaptcha">
-                            <div class="modal-dialog modal-sm">
-                                <div class="modal-content">
-                                    <div class="modal-body">
-                                        <h4 style="font-size: 14px;"> We think you are a robot. Prove us wrong if you think if you are not.</h4>
-                                    </div>
-                                    <div class="modal-footer"> <button type="button" class="btn btn-success" onclick="closemodalrecaptcha()"> OK </button></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>FOOD CLUB</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+ 
+
+  
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+
+
+
+ 
+</head>
+
+<body class="no-js">
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <h1 class="text">FOOD CLUB</h1>
+      </div>
+
+    
+       
+<nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#Food">Restaurant</a></li>
+          <li><a href="#footer">Contact Us</a></li>
+          <li class="get-started"><a href="signin.php">Sign In</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+          <div class="container">
+           
+          <div class="back">
+          <img class="img" src="images/imgg.png" alt=""/>
+          </div>
+           
+          </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+  <!-- ======= into ======= -->
+  <section id="intro">
+    <div class="intro">
+    <div><img class="intro2" src="images/food.jpg" alt=""></div>
+    <div class="cont">
+      <p style="font-size: 30px;">You've got stuff to do.</p>
+      <p style="font-size: 30px;">We've got Options.</p>
+      <p2 style="font-size: 19px;">Get it delivered right to your door. Or, try</p2>
+      <p2 style="font-size: 19px;">Pickup on your way home.It's mealtime on your time.</p2><br>
+      <input type="button" class="order"  value="Order now">
+    </div>
+    </div>
+  </section>
+    <!-- ======= Food Section ======= -->
+    <section id="Food" class="Food">
+      <a class="food" style="font-size: 30px;">Food Near You</a>
+      <div class="cards-list">
+        
+        <div class="card 1">
+          
+          <div class="card_image">
+           <img src="images/img1.jpg" />
             </div>
-            <script src="js/particles.js"></script> 
-            <script defer src="js/index.js"></script> 
-            <script src="js/jquery.min.js"></script>
-            <script>
-                                        var _captchaTries = 0;
-                                        function recaptchaOnload() {
-                                        _captchaTries++;
-                                                if (_captchaTries > 9)
-                                                return;
-                                                if ($('.g-recaptcha').length > 0) {
-                                        grecaptcha.render("recaptcha", {sitekey: '6LcmWcoUAAAAAI6AHCSSZCQFbovJ8opgL1AYPYG-', callback: function () {
-                                        console.log('recaptcha callback');
-                                        }});
-                                                return;
-                                        }
-                                        window.setTimeout(recaptchaOnload, 1000);
-                                        }
-                                        $.ajax({url: "ajaxCheckSession.php", success: function(result){if (result == 1){window.location.replace('main.php'); }});
-            </script> 
-        </body>
-    </html>
-    <?php
-} 
+          <div class="card_title title-white">
+            <p>American</p>
+          </div>
+        </div>
+        
+          <div class="card 2">
+          <div class="card_image">
+            <img src="images/img2.jpg" />
+            </div>
+          <div class="card_title title-white">
+            <p>Italian</p>
+          </div>
+        </div>
+        
+        <div class="card 3">
+          <div class="card_image">
+            <img src="images/img3.jpg" />
+          </div>
+          <div class="card_title title-white">
+            <p>Indian</p>
+          </div>
+          </div>
+
+          <div class="card 3">
+            <div class="card_image">
+              <img src="images/img4.jpg" />
+            </div>
+            <div class="card_title title-white">
+              <p>Chinese</p>
+            </div>
+            </div>
+
+          </div>
+    </section><!-- End Food Section -->
+
+    
+     
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <section id="footer">
+
+    <div class="section-title">
+      <h2>About Us</h2>
+    </div>
+    
+    <div class="footer-top">
+      <div class="container">
+        <div class="row" style="margin:15px">
+
+          <div class="about" data-aos="fade-up">
+            <h3>FOOD CLUB</h3>
+            <p>
+              Gandhi nagar <br>
+              Madukkarai,coimbatore<br>
+              Tamil nadu <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+            <br>
+          </div>
+
+          <div class="about1" data-aos="fade-up" data-aos-delay="100">
+            <h4>Useful Links</h4>
+            <ul style="list-style-type:none;">
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+            <br>
+          </div>
+
+         
+
+          <div class="about2" data-aos="fade-up" data-aos-delay="300">
+            <h4>Our Social Networks</h4>
+            <p>Follow us on the social media  </p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="skype"><i class="bx bxl-skype"></i></a>
+            </div>
+            <br>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>FOOD CLUB</span></strong>. All Rights Reserved
+      </div>
+     
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+    </section>
+  <!-- Vendor JS Files -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+  <script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="vendor/venobox/venobox.min.js"></script>
+  <script src="vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+
+
+
+  
+
+</body>
+
+</html>
+
