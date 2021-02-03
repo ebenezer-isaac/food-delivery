caefbebe9592fe26ce4f89e04e5afebceeb0f8f3
gowshikkumar <77010614+gowshikkumar@users.noreply.github.com>
2021-02-03 16:57:44 +0530

    dish filter
    added type taste and category

--- WebApp/search.php
+++ WebApp/search.php
@@ -1,8 +1,56 @@
 Enter the Dish Name that you want to search for : <input type="text" onkeyup="javascript:getSearch()" id="search_text">
+
+
+Cuisine : <select id="mySelect1" onchange="myFunction1()" style="padding:3px 5px;">
+  <option value="">select..
+  <option value="Italian">Italian
+  <option value="Indian">Indian
+  <option value="American">American
+  <option value="Chinese">Chinese
+</select>
+
+Type : <select id="mySelect2"  onchange="myFunction2()" style="padding:3px 5px;">
+  <option value="">select..
+  <option value="Veg">Veg
+  <option value="Non-Veg">Non-Veg
+</select>
+
+Taste : <select id="mySelect3" onchange="myFunction3()" style="padding:3px 5px;">
+  <option value="">select..
+  <option value="Sour">Sour
+  <option value="Salty">Salty
+  <option value="Spicy">Spicy
+  <option value="Sweet">Sweet
+</select>
+
+
+
 <br>
 <br>
 <div id='result'></div>
 <script>
+function myFunction1()
+{
+	var x = document.getElementById("mySelect1").value;
+  $.get("dis_cui_search.php?search1="+x, function(data, status){
+    		document.getElementById("result").innerHTML = "<div class='row'>"+data+"</div>";
+		  });
+}
+	function myFunction2() {
+	
+  var y = document.getElementById("mySelect2").value;
+  $.get("dis_typ_search.php?search2="+y, function(data, status){
+    		document.getElementById("result").innerHTML = "<div class='row'>"+data+"</div>";
+		  });}
+
+		  function myFunction3() {
+		  var z = document.getElementById("mySelect3").value;
+  $.get("dis_tas_search.php?search3="+z, function(data, status){
+    		document.getElementById("result").innerHTML = "<div class='row'>"+data+"</div>";
+		  });		  
+
+}
+
 	function getSearch(){
 		var search = document.getElementById("search_text").value;
 		$.get("dish_search.php?search="+search+"&cat=veg", function(data, status){
