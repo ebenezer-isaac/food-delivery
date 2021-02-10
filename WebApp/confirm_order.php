502fe2ef2b41367c1b72e5be4be339687ad53ed4
Ebenezer Isaac <mail@ebenezer-isaac.com>
2021-02-10 10:39:24 +0530

    order confirmation data retrieval

--- WebApp/confirm_order.php
+++ WebApp/confirm_order.php
@@ -2,7 +2,9 @@
 if(isset($_COOKIE['res_id']) && isset($_COOKIE["cart_obj"])) {
 	$res_id = $_COOKIE['res_id'];
 	$cart = $_COOKIE["cart_obj"];
-	echo $cart;
+	echo "res_id : ".$res_id."<br>";
 	$cart = json_decode($cart);
-	print_r($cart);
+	foreach($cart as $dish_id =>$qty){
+		echo $dish_id." : ".$qty."<br>";
+	}
 }
