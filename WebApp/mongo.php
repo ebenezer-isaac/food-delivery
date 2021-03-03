b6a97788ce82994ba8699ef303e2269c9bdc3dc1
gowshikkumar <77010614+gowshikkumar@users.noreply.github.com>
2021-03-03 18:13:03 +0530

    Update mongo.php

--- WebApp/mongo.php
+++ WebApp/mongo.php
@@ -1,12 +1,17 @@
 <?php
-try {
 
-    $mng = new MongoDB\Driver\Manager("mongodb+srv://food_delivery:contech%402021@food-delivery.3ukn0.mongodb.net/food_delivery?authSource=admin&replicaSet=atlas-o0xpuf-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
-echo"connection established";
+$manager = new MongoDB\Driver\Manager("mongodb+srv://food_delivery:contech%402021@food-delivery.3ukn0.mongodb.net/food_delivery?authSource=admin&replicaSet=atlas-o0xpuf-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
 
-    }
-catch(Exception $e)
-{
-echo $e;
+$query = new MongoDB\Driver\Query([]);
+
+$cursor = $manager->executeQuery("food_delivery.food_delivery");
+
+
+foreach($cursor as $document){
+
+    var_dump($document);
 }
+
 ?>
+
+
