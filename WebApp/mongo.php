d1dd0fbf999ffb4e657206e8f04ea5bf085d67d6
gowshikkumar <77010614+gowshikkumar@users.noreply.github.com>
2021-03-03 17:10:13 +0530

    php mongodb connection

---
+++ WebApp/mongo.php
@@ -0,0 +1,12 @@
+<?php
+try {
+
+    $mng = new MongoDB\Driver\Manager("mongodb+srv://food_delivery:contech%402021@food-delivery.3ukn0.mongodb.net/food_delivery?authSource=admin&replicaSet=atlas-o0xpuf-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
+echo"connection established";
+
+    }
+catch(Exception $e)
+{
+echo $e;
+}
+?>
